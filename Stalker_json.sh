160b0e6aa323e3d7caed57b565cd9a567189a925
Said-Pro <haddawi0924@gmail.com>
2025-09-09 14:30:20 +0200

    Update Stalker_json.sh

--- Stalker_json.sh
+++ Stalker_json.sh
@@ -5,4 +5,5 @@ echo "             update: 09/09/2025"
 echo "=============================================="
 echo ""
 
-wget -O /usr/lib/enigma2/python/Plugins/Extensions/Uni_Stalker/Servers/Stalker.json https://raw.githubusercontent.com/MARKETTV1/union/refs/heads/main/Stalker.json && killall -9 enigma2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/Uni_Stalker/Servers/Stalker.json https://raw.githubusercontent.com/Said-Pro/Uni_Stalker/refs/heads/main/Stalker.json && killall -9 enigma2
