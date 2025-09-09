3141356bb20e59b272908c376eb8d9a537d8615e
Said-Pro <haddawi0924@gmail.com>
2025-09-09 14:19:44 +0200

    Update Stalker_json.sh

--- Stalker_json.sh
+++ Stalker_json.sh
@@ -1,1 +1,8 @@
+clear
+echo "=============================================="
+echo "             list stalker.json"
+echo "             update: 09/09/2025"
+echo "=============================================="
+echo ""
 
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/Uni_Stalker/Servers/Stalker.json https://raw.githubusercontent.com/MARKETTV1/union/refs/heads/main/Stalker.json && killall -9 enigma2
